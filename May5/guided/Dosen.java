9e84fbbb45c544b87b1088e150fca26837f25fa8
agungibr <agungmalik050@gmail.com>
2025-03-11 17:32:27 +0700

    feat: initial template

---
+++ May5/guided/Dosen.java
@@ -0,0 +1,7 @@
+package May5.guided;
+
+public class Dosen {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, Dosen!");
+    }
+}
---
+++ May5/guided/Mahasiswa.java
@@ -0,0 +1,7 @@
+package May5.guided;
+
+public class Mahasiswa {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, Mahasiswa!");
+    }
+}
